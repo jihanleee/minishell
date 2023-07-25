3a46975ab74e74600cb72417ab8d86fe5477e401
Hea in SONG <heain.song328@gmail.com>
2023-07-25 18:56:12 +0200

    find_param.c

---
+++ srcs/find_param.c
@@ -0,0 +1,23 @@
+#include "minishell.h"
+
+char	*find_param(char *p_name, char **envp)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], p_name, ft_strlen(p_name)) == 0)
+		{
+			if (envp[i][ft_strlen(p_name)] == '=')
+			{
+				str = ft_strdup(ft_strchr(envp[i], '=') + 1);
+				printf("%s\n", str);
+				break ;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
