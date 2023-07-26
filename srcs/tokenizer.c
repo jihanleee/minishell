f6623d3cfd07e979eb4ed3e5470fdb7252285515
jihalee <jihalee@e3r8p16.clusters.42paris.fr>
2023-07-26 15:22:35 +0200

    removed main from tokenizer.c

--- srcs/tokenizer.c
+++ srcs/tokenizer.c
@@ -249,26 +249,6 @@ t_token	*create_tokens(char *line)
 	return (free(quote_info), tokens);
 }
 
-/* int	main(int argc, char **argv, char **envp)
-{
-	t_token	*tokens;
-	t_token	*temp;
-	char	*line;
-	int		*info;
-	int		i;
-	char	*tok;
-
-	while (1)
-	{	temp = tokens;
-		line = readline("%");
-		ft_printf("line : %s\n", line);
-		tokens = create_tokens(line);
-		if (check_tokens(tokens) != 0)
-			exit_error("bash: syntax error\n", &temp);
-	}
-	return (0);
-} */
-
 //token type
 //0: word
 //1: <
