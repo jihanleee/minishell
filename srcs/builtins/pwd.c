a3a4c460c1f5c79e2e10efeb2a5cc0199063ec3f
Soye LEE <solee2@paul-f4Br9s1.clusters.42paris.fr>
2023-08-13 00:33:38 +0200

    pwd norm done

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -1,19 +1,5 @@
 #include "minishell.h"
 
-//static functions should be deleted later as 
-//they already exist in another file + header file
-
-/*static int	get_length(char *str)
-{
-	int	length;
-
-	length = 0;
-	while (str[length])
-		length++;
-	return (length);
-}
-*/
-
 int	ft_pwd(t_job **lst, char **env, int fd)
 {
 	t_job	*current;
@@ -21,16 +7,14 @@ int	ft_pwd(t_job **lst, char **env, int fd)
 	int		length;
 
 	current = *lst;
+	/*
 	if (current->arg)	//pwd should not take any arguments after it
 	{
+		//pwd뒤에 뭐가 들어오면 안된다고 생각했는데 보니까 pwd는 뒤에 뭐가 와도 그냥 작동하는듯..?
 		printf("pwd: too many arguments\n");
-		//return int that indicates an error
-		//error message = "pwd: too many arguments\n"
 	}
-	//print = find_param(current->cmd, env);
+	*/
 	print = getcwd(NULL, 0);
-	//if (!print)
-	//	return (1);
 	length = get_length(print);
 	ft_putstr_fd(print, fd);
 	write(fd, "\n", 1);
