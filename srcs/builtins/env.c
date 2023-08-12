4c19f88cc7394c03fe583e42355e3db34885f779
Soye LEE <solee2@paul-f4Br9s1.clusters.42paris.fr>
2023-08-12 23:15:40 +0200

    env norm done

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -7,29 +7,19 @@ void	ft_env(t_job **lst, char **env, int fd)
 	t_job	*current;
 
 	current = *lst;
-	printf("inside env function\n\n");
 	i = 0;
-	//if (current->arg[0] != NULL)
 	if (current->arg)
 	{
-		printf("env - too many arguments error detected\n");
-		//printf("1st line of env has \n\t%s\n", env[i]);
+		write(fd, "env: \'", 6);
+		write(fd, current->arg[0], get_length(current->arg[0]));
+		write(fd, "\': No such file or directory\n", 29);
 		return ;
-		//error
-		//error message: 
-			//env: ‘(first arg)’: No such file or directory
 	}
-	//printf("before this line\n");
-	//printf("1st line of env has \n\t%s\n", env[i]);
-	//printf("after this line\n");
-	//printf("%dth line of env has %s\n", i + 1, env[i]);
 	while (env[i] && env[i][0])
 	{
-		//printf("\tno seg fault for %d\n", i);
 		length = get_length(env[i]);
 		write(fd, env[i], length);
 		write(fd, "\n", 1);
 		i++;
 	}
-	//printf("env has index %d\n", i);
 }
